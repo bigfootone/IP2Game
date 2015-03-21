69a9a8956d3f43491b05709308ba82d6dab1a057
Adam17x <adam_uddin@live.co.uk>
2015-03-21 23:01:48 +0000

    For enemy projectile

---
+++ Projectile.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+
+ var targ: GameObject;
+ targ = GameObject.FindWithTag("Player");
+ 
+function Start () {
+transform.position = Vector3.MoveTowards(transform.position, targ.transform.position, .5);
+
+}
+
+
+    
+function Update () {
+
+}
