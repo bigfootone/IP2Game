8a691ba6bd5e4de29b511a097e26dcc5d6586e7a
Adam17x <adam_uddin@live.co.uk>
2015-03-21 20:53:05 +0000

    EnemyRotation
    Rotates enemy so projectile can be fired forward towards player

---
+++ EnemyRotation.js
@@ -0,0 +1,13 @@
+﻿  #pragma strict
+    var target : Transform;
+    
+    function Start () {
+    
+    target = GameObject.FindWithTag("Player").transform;
+    }
+    
+    function Update () 
+    {
+     
+	transform.LookAt(transform.position + new Vector3(0,0,1), target.transform.position - transform.position);  
+	}
