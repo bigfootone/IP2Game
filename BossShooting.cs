b231f9c97f4abd36fbde34f5851190f5de2b684b
Adam17x <adam_uddin@live.co.uk>
2015-04-05 17:19:56 +0100

    Shoots in 8 directions
    Keep enemyrotation script on boss will achieve what we want. Make boss
    appear to shoot randomly in 8 different directions

---
+++ BossShooting.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossShooting : MonoBehaviour {
+	
+	public Rigidbody2D projectile;
+	private float fireRate = 0.9f;
+	private float nextShot = 0;
+	private float bulletSpeed = 600f;
+	
+		
+	// Use this for initialization
+	void Start () {	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		Rigidbody2D shooting;
+
+		if (Time.time > nextShot)
+		{
+		nextShot = Time.time + fireRate;
+
+		shooting = Instantiate (projectile, transform.position,Quaternion.Euler(0,0,-90.0f)) as Rigidbody2D;
+		shooting.velocity = GetComponent<Rigidbody2D>().velocity/3;
+		shooting.AddForce (transform.up *-bulletSpeed);
+
+		shooting = Instantiate (projectile, transform.position,Quaternion.Euler (0,0,90.0f)) as Rigidbody2D;
+		shooting.velocity = GetComponent<Rigidbody2D>().velocity/3;
+		shooting.AddForce (transform.up * bulletSpeed);
+
+		shooting = Instantiate (projectile, transform.position,transform.rotation) as Rigidbody2D;
+		shooting.velocity = GetComponent<Rigidbody2D>().velocity/3;
+		shooting.transform.rotation = Quaternion.AngleAxis (180,Vector3.down);
+		shooting.AddForce (transform.right *-bulletSpeed);
+
+		shooting = Instantiate (projectile, transform.position,transform.rotation) as Rigidbody2D;
+		shooting.velocity = GetComponent<Rigidbody2D>().velocity/3;
+		shooting.AddForce (transform.right *bulletSpeed);
+
+
+		shooting = Instantiate (projectile, transform.position,Quaternion.Euler(0,0,-90.0f)) as Rigidbody2D;
+		shooting.velocity = GetComponent<Rigidbody2D>().velocity/3;
+		shooting.AddForce (transform.up *-bulletSpeed);
+		shooting.AddForce (transform.right *bulletSpeed);
+
+		shooting = Instantiate (projectile, transform.position,Quaternion.Euler(0,0,-90.0f)) as Rigidbody2D;
+		shooting.velocity = GetComponent<Rigidbody2D>().velocity/3;
+		shooting.AddForce (transform.up *-bulletSpeed);
+		shooting.AddForce (transform.right *-bulletSpeed);
+
+		shooting = Instantiate (projectile, transform.position,Quaternion.Euler(0,0,-90.0f)) as Rigidbody2D;
+		shooting.velocity = GetComponent<Rigidbody2D>().velocity/3;
+		shooting.AddForce (transform.up *bulletSpeed);
+		shooting.AddForce (transform.right *bulletSpeed);
+		
+		shooting = Instantiate (projectile, transform.position,Quaternion.Euler (0,0,90.0f)) as Rigidbody2D;
+		shooting.velocity = GetComponent<Rigidbody2D>().velocity/3;
+		shooting.AddForce (transform.up * bulletSpeed);
+		shooting.AddForce (transform.right *-bulletSpeed);
+
+		}
+	
+	}
+	
+}
